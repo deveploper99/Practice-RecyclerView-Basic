37a9772484aa9e61cf6f98c3857bb3ac4abf59c8
deveploper99 <rtxdeveploper99@gmail.com>
2025-10-22 19:39:04 +0600

    circle image view create

--- app/src/main/java/com/example/recyclerview/StudentAdapter.kt
+++ app/src/main/java/com/example/recyclerview/StudentAdapter.kt
@@ -1,4 +1,29 @@
 package com.example.recyclerview
 
-class StudentAdapter {
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class StudentAdapter(private val StudentList: List<Student>) : RecyclerView.Adapter<StudentAdapter.StudentViewHolder>() {
+
+    class StudentViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+    }
+
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): StudentAdapter.StudentViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: StudentAdapter.StudentViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
 }
